7eff88321f97392fc781d250ce5a2ad16f67f2d3
WaterNinja101 <coolwolfs8@gmail.com>
2021-10-22 23:51:35 -0400

    Removed double header

--- db/src/routes/account/[id].svelte
+++ db/src/routes/account/[id].svelte
@@ -34,15 +34,6 @@
 </svelte:head>
 
 <main>
-    <DatabaseHeader>
-        {#if $session.isLoggedIn}
-            <h1 style="margin: 0;">{$session.userData?.username}</h1>
-        {:else}
-            <a href={`https://discord.com/api/oauth2/authorize?client_id=895468421054083112&redirect_uri=http%3A%2F%2F${encodeURIComponent(HOST_URL)}%2Fauth%2Faccount&response_type=code&scope=identify`}>
-                <button>Login</button>
-            </a>
-        {/if}
-    </DatabaseHeader>
     {#if !$session.isLoggedIn}
         <NotLoggedIn page="account"/>
     {:else if !$session.userID}
