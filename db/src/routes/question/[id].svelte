ea1717ad3b4781448894715be0228409d7e75e59
WaterNinja101 <coolwolfs8@gmail.com>
2021-10-23 18:01:30 -0400

    Removed double header

--- db/src/routes/question/[id].svelte
+++ db/src/routes/question/[id].svelte
@@ -17,7 +17,6 @@
     import type {SaQuestion, McqQuestion} from 'src/mongo'
     import Question from '$lib/components/Question.svelte';
     import Cookie from 'js-cookie'
-    import DatabaseHeader from '$lib/components/DatabaseHeader.svelte';
     import NotLoggedIn from "$lib/components/NotLoggedIn.svelte";
     import NotAuthorized from "$lib/components/NotAuthorized.svelte";
     import { HOST_URL } from '$lib/variables';
@@ -77,15 +76,6 @@
 }}></svelte:body>
 
 <main>
-    <DatabaseHeader>
-        {#if $session.isLoggedIn}
-            <h1 style="margin: 0;">{$session.userData?.username}</h1>
-        {:else}
-            <a href={`https://discord.com/api/oauth2/authorize?client_id=895468421054083112&redirect_uri=http%3A%2F%2F${encodeURIComponent(HOST_URL)}%2Fauth%2Fquestion-search&response_type=code&scope=identify`}>
-                <button>Login</button>
-            </a>
-        {/if}
-    </DatabaseHeader>
     {#if !$session.isLoggedIn}
         <NotLoggedIn page="question-search" />
     {:else if !$session.userData?.username}
