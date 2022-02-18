39fd44e7e519ff6fe8e968b1198e9ebf6e0e0b46
Bomjoe <bob.omjoe@gmail.com>
2022-02-17 23:56:08 -0500

    Added alot of stuff to esbot

---
+++ home/src/routes/edit/index.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    import type { LoadInput } from '@sveltejs/kit'
+
+    export async function load({ session, fetch }: LoadInput) {
+        const teamsRes = session.userData.teamIds.map(t => await fetch())
+    }
+</script>
+
+<script lang="ts">
+    let selectedTeam = null
+</>
+
+<main>
+    
+</main>
